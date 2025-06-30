98534fb0d083f90b6234f98508a3a475ff99c0d3
BINEESHA KP <bineeshaharilal@gmail.com>
2025-06-30 20:25:15 +0530

    Add files via upload

--- code/data_analysis.py
+++ code/data_analysis.py
@@ -1,1 +1,16 @@
+# Data Analysis on CSV File
+# Author  : Bineesha K P
+# Date    : 30-06-2025
 
+import pandas as pd
+import matplotlib.pyplot as plt
+
+df = pd.read_csv('sales.csv')
+grouped = df.groupby('Product')['Sales'].sum()
+grouped.plot(kind='bar')
+
+plt.xlabel('Product')
+plt.ylabel('Total Sales')
+plt.title('Sales by Product')
+plt.tight_layout()
+plt.show()
